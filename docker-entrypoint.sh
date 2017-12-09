1974bf1ebde3a78ed0fba19e8f264fae8ed4a285
Bekhzod Tillakhanov <bekhzod.tillakhanov@gmail.com>
2017-12-10 04:53:01 +0500

    CI integration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,25 +6,29 @@ export PROJECT_DIR=/var/www/unkata-ui
 # Go to directory
 cd $PROJECT_DIR
 
-# Change permission
-chmod 775 -R $PROJECT_DIR/dist
-chown app:app -R $PROJECT_DIR/dist
-
 # Fix go su problem
 export HOME=/home/app
 
-gosu app bash
-
-source ~/.bashrc
-
 if [[ $NODE_ENV == 'testing' ]]; then
     echo "Run testing mode"
+
+    # Change user
+    gosu app bash
+    source ~/.bashrc
 
     # Run test
     xvfb-run --server-args='-screen 0, 1024x768x16' yarn test
     exit
 else
     echo "Run production mode"
+
+    # Change permission
+    chmod 775 -R $PROJECT_DIR/dist
+    chown app:app -R $PROJECT_DIR/dist
+
+    # Change user
+    gosu app bash
+    source ~/.bashrc
 
     # Build source
     yarn build

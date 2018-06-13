423701d45e7b88b6a7e378fcae8a3c1b3dd8888a
Bekhzod Tillakhanov <bekhzod.tillakhanov@gmail.com>
2018-06-13 05:25:21 +0500

    Fix ci

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ if [[ $NODE_ENV == 'testing' ]]; then
     echo "Run testing mode"
 
     # Run test
-    gosu app source ~/.bashrc && xvfb-run --server-args='-screen 0, 1024x768x16' yarn test
+    gosu app xvfb-run --server-args='-screen 0, 1024x768x16' yarn test
     exit
 else
     echo "Run production mode"
@@ -21,7 +21,7 @@ else
     chown app:app -R $PROJECT_DIR/dist
 
     # Build source
-    gosu app source ~/.bashrc && yarn run clean
-    gosu app source ~/.bashrc && yarn build
+    gosu app yarn run clean
+    gosu app yarn build
     exit
 fi
